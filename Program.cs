f53489464ad1b6fa61c2bd685c4b59e468294e7c
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2021-10-05 19:35:33 +1000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Converter
             System.Collections.Generic.List<string> arguments = new System.Collections.Generic.List<string>(args);
             if (arguments.Contains("-koef"))
             {
-               
+
                 //convert koef file
                 Console.WriteLine("Trying to find koef file...");
 
@@ -80,7 +80,7 @@ namespace Converter
                         }
                         csv.Append($"\n");
                         Console.CursorLeft = ("Reading Machine emition data: ").Length;
-                        Console.Write($"{(int)(((float)(i + 51 )/ (float)strings.Length)*100f)}%");
+                        Console.Write($"{(int)(((float)(i + 51) / (float)strings.Length) * 100f)}%");
                         //Console.Write($"\n");
 
                     }
@@ -100,7 +100,7 @@ namespace Converter
             }
             Console.WriteLine();
             if (arguments.Contains("-tp"))
-            {       
+            {
                 //convert koef file
                 Console.WriteLine("Trying to find tp file...");
                 //read machine names
@@ -112,13 +112,13 @@ namespace Converter
                     System.Console.Write("Reading machines : ");
                     System.Text.StringBuilder csv = new System.Text.StringBuilder();
                     byte[] strings = System.IO.File.ReadAllBytes(arguments[arguments.IndexOf("-tp") + 1]);
-                    System.Collections.Generic.Dictionary<double,System.Collections.Generic.List<byte>> res = new System.Collections.Generic.Dictionary<double, System.Collections.Generic.List<byte>>(4);
+                    System.Collections.Generic.Dictionary<double, System.Collections.Generic.List<byte>> res = new System.Collections.Generic.Dictionary<double, System.Collections.Generic.List<byte>>(4);
                     try
                     {
                         for (int i = 0; i < strings.Length; i += 526)
                         {
                             int id = Convert.ToInt32(strings[i]);
-                            
+
                             //read till we hit more then one space in the row
                             int spacecount = 0;
                             int charCount = -16;//because 0 is the machine id
@@ -139,28 +139,28 @@ namespace Converter
                                     charCount++;
                                 }
                             }
-                            catch(System.IndexOutOfRangeException)
+                            catch (System.IndexOutOfRangeException)
                             {
-                               tableId.Add(49);
+                                tableId.Add(49);
                             }
                             double group = Convert.ToDouble(System.Text.Encoding.Default.GetString(tableId.ToArray()), System.Globalization.CultureInfo.InvariantCulture);
                             bool need = true;
 
-                            foreach(var key in res.Keys)
+                            foreach (var key in res.Keys)
                             {
-                                if( group == key)
+                                if (group == key)
                                 {
                                     need = false;
                                 }
                             }
-                            if(need)
+                            if (need)
                             {
                                 res.Add(group, new System.Collections.Generic.List<byte>());
                                 Console.WriteLine($"Added group : {group}");
                             }
 
                             res[group].Add(strings[i]);
-                            
+
                             res[group].Add(59);
                             csv.Append($"{id};");
                             spacecount = 0;
@@ -209,11 +209,11 @@ namespace Converter
                     {
                         //ignore that
                     }
-                    foreach(var dat in res)
+                    foreach (var dat in res)
                     {
                         System.IO.File.WriteAllBytes($"./machines_{dat.Key}.csv", dat.Value.ToArray());
                     }
-                    
+
                 }
                 else
                 {
@@ -235,6 +235,67 @@ namespace Converter
                     System.Text.StringBuilder csv = new System.Text.StringBuilder();
                     byte[] strings = System.IO.File.ReadAllBytes(arguments[arguments.IndexOf("-zv") + 1]);
 
+                    System.Collections.Generic.List<byte> resBytes = new System.Collections.Generic.List<byte>();
+                    for (int i = 0; i < strings.Length; i += 288)
+                    {
+                        try
+                        {
+                            int count = 0;
+                            //first few bytes(it appears as if the number does not actually have two bytes)
+                            //id is stored in first 12 bytes
+                            //number -> read the value(number is less then or equal to two digits)
+                            byte[] idBytes = new byte[12];
+                            byte[] codeBytes = new byte[4];
+                            byte[] nameBytes = new byte[258];
+                            byte[] valueBytes = new byte[12];
+                            byte solid = 0x54;
+                            for (int j = 0; j < 12; j++)
+                            {
+                                idBytes[j] = strings[i + j];
+                            }
+
+                            //code is stored as next 4 bytes
+                            for (int j = 0; j < 4; j++)
+                            {
+                                codeBytes[j] = strings[i + j + 12];
+                            }
+
+                            //name is stored as next 258 bytes
+                            //we also need to clean empty space from the name
+                            for (int j = 0; j < 258; j++)
+                            {
+                                nameBytes[j] = strings[i + j + 16];
+                            }
+
+                            for (int j = 0; j < 12; j++)
+                            {
+                                valueBytes[j] = strings[i + j + 273];
+                            }
+
+                            //add to string builder
+                            csv.Append(
+                                $"{System.Text.Encoding.UTF8.GetString(idBytes)};" +
+                                $"{System.Text.Encoding.UTF8.GetString(codeBytes)};" +
+                                $"{System.Text.Encoding.UTF8.GetString(nameBytes)};" +
+                                $"{System.Text.Encoding.UTF8.GetString(valueBytes)}" +
+                                $"{solid}\n");
+                            resBytes.AddRange(idBytes);
+                            resBytes.Add(59);
+                            resBytes.AddRange(codeBytes);
+                            resBytes.Add(59);
+                            resBytes.AddRange(nameBytes);
+                            resBytes.Add(59);
+                            resBytes.AddRange(valueBytes);
+                            resBytes.Add(59);
+                            resBytes.Add(strings[i+285]);
+                            resBytes.Add(10);
+                        }
+                        catch (System.IndexOutOfRangeException e)
+                        {
+
+                        }
+                    }
+                    System.IO.File.WriteAllBytes($"./elements.csv", resBytes.ToArray());
                 }
             }
         }
